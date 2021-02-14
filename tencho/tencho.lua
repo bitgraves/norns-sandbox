ee55468434edd611abe7e9dc9e30220ae637a6c4
Ben <spongebucket@gmail.com>
2021-02-15 02:49:29 +0700

    migrate tencho

--- tencho/tencho.lua
+++ tencho/tencho.lua
@@ -2,47 +2,37 @@
 -- is techno spelled wrong
 
 local BGUtil = include('bitgraves/common/bgutil')
+local BGMidi = include('bitgraves/common/bgmidi')
 local Hexagon = include('bitgraves/common/hexagon')
 
 engine.name = 'Tencho'
 mid = nil
 
 local indexOffset = 2
+local MPD218
 
 function init()
-  audio:rev_off() -- no system reverb
-  audio:pitch_off() -- no system pitch analysis
-  audio:monitor_stereo() -- expect stereo input
+  BGUtil.configureSystemStuff()
 
-  params:add_control("amp", "amp", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("amp", function(x)
-    engine.amp(x)
-  end)
+  BGUtil.addEngineControlParam(params, { id = "amp" })
+  BGUtil.addEngineControlParam(params, { id = "chopR" })
+  BGUtil.addEngineControlParam(params, { id = "chopTimeR", min = 0.3, max = 0.7 })
+  BGUtil.addEngineControlParam(params, { id = "delayR" })
+  BGUtil.addEngineControlParam(params, { id = "noise", max = 0.25 })
   
   params:add_control("monitor", "monitor", controlspec.new(0, 1, 'lin', 0, 0, ''))
   params:set_action("monitor", function(x)
     audio.level_monitor(x)
   end)
   
-  params:add_control("chopR", "chopR", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("chopR", function(x)
-    engine.chopR(x)
-  end)
-  
-  params:add_control("chopTimeR", "chopTimeR", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("chopTimeR", function(x)
-    engine.chopTimeR(util.linlin(0, 1, 0.3, 0.7, x))
-  end)
-  
-  params:add_control("delayR", "delayR", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("delayR", function(x)
-    engine.delayR(x)
-  end)
-  
-  params:add_control("noise", "noise", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("noise", function(x)
-    engine.noise(util.linlin(0, 1, 0, 0.25, x))
-  end)
+  MPD218 = BGMidi.newInputMappingMPD218({
+    [3] = 'noise',
+    [9] = 'delayR',
+    [12] = 'chopR',
+    [13] = 'chopTimeR',
+    [14] = 'monitor',
+    [15] = 'amp',
+  })
   
   --[[
     this.addCommand("chopR", "f", {|msg|
@@ -59,47 +49,6 @@ function init()
   redraw()
 end
 
-function enc(nEnc, delta)
-
-end
-
--- mapping from Akai MPD218 knobs to param handlers
-local ccAkaiMapping = {
-  [3] = 'noise',
-  [9] = 'delayR',
-  [12] = 'chopR',
-  [13] = 'chopTimeR',
-  [14] = 'monitor',
-  [15] = 'amp',
-}
-
-local ccHandlers = {
-  ['noise'] = function(val)
-    params:set('noise', val)
-    return 'noise ' .. val
-  end,
-  ['chopR'] = function(val)
-    params:set('chopR', val)
-    return 'chopR ' .. val
-  end,
-  ['chopTimeR'] = function(val)
-    params:set('chopTimeR', val)
-    return 'chop time ' .. val
-  end,
-  ['delayR'] = function(val)
-    params:set('delayR', val)
-    return 'delayR ' .. val
-  end,
-  ['monitor'] = function(val)
-    params:set('monitor', val)
-    return 'monitor ' .. val
-  end,
-  ['amp'] = function(val)
-    params:set('amp', val)
-    return 'amp ' .. val
-  end,
-}
-
 function midiEvent(data)
   -- tab.print(midi.to_msg(data))
   local d = midi.to_msg(data)
@@ -113,14 +62,13 @@ function midiEvent(data)
   elseif d.type == 'note_off' then
     local index = d.note - 36
   elseif d.type == 'cc' then
-    local handler = ccAkaiMapping[d.cc]
-    if handler ~= nil and ccHandlers[handler] ~= nil then
-      local msg = ccHandlers[handler](d.val / 127)
+    local handled, msg = BGMidi.handleCCMPD218(MPD218, params, d.cc, d.val)
+    if handled then
       redraw(msg)
     end
   end
 end
 
 function redraw(msg)
-  Hexagon:draw(msg, ccAkaiMapping)
+  Hexagon:drawFancy(MPD218, msg)
 end
