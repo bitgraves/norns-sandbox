644b77e8cde456759d4aaa2f844f3dd7d4a7f996
Ben <spongebucket@gmail.com>
2021-02-15 02:54:51 +0700

    migrate bounce

--- bounce/bounce.lua
+++ bounce/bounce.lua
@@ -1,46 +1,36 @@
 -- bounce
 
 local BGUtil = include('bitgraves/common/bgutil')
+local BGMidi = include('bitgraves/common/bgmidi')
 local Hexagon = include('bitgraves/common/hexagon')
 
 engine.name = 'Bounce'
 local isOther = 0
 mid = nil
+local MPD218
 
 function init()
-  audio:rev_off() -- no system reverb
-  audio:pitch_off() -- no system pitch analysis
-  audio:monitor_mono() -- expect only channel 1 input
+  BGUtil.configureSystemStuff()
 
-  params:add_control("amp", "amp", controlspec.new(0, 1, 'lin', 0, 1, ''))
-  params:set_action("amp", function(x)
-    engine.amp(x)
-  end)
-
-  params:add_control("bounce", "bounce", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("bounce", function(x)
-    engine.bounce(x)
-  end)
-
-  params:add_control("drift", "drift", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("drift", function(x)
-    engine.drift(x)
-  end)
-
-  params:add_control("lpf", "lpf", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("lpf", function(x)
-    engine.lpf(x)
-  end)
-
-  params:add_control("hiTexAmp", "hiTexAmp", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("hiTexAmp", function(x)
-    engine.hiTexAmp(x)
-  end)
+  BGUtil.addEngineControlParam(params, { id = "amp" })
+  BGUtil.addEngineControlParam(params, { id = "bounce" })
+  BGUtil.addEngineControlParam(params, { id = "drift" })
+  BGUtil.addEngineControlParam(params, { id = "lpf" })
+  BGUtil.addEngineControlParam(params, { id = "hiTexAmp" })
   
   params:add_control("monitor", "monitor", controlspec.new(0, 1, 'lin', 0, 0, ''))
   params:set_action("monitor", function(x)
     audio.level_monitor(x)
   end)
+  
+  MPD218 = BGMidi.newInputMappingMPD218({
+    [3] = 'hiTexAmp',
+    [9] = 'bounce',
+    [12] = 'drift',
+    [13] = 'lpf',
+    [14] = 'monitor',
+    [15] = 'amp',
+  })
   
   mid = midi.connect()
   mid.event = midiEvent
@@ -59,43 +49,6 @@ end
 function key(...)
   BGUtil.setlist_key('bounce/bounce', ...)
 end
-
--- mapping from Akai MPD218 knobs to param handlers
-local ccAkaiMapping = {
-  [3] = 'hiTexAmp',
-  [9] = 'bounce',
-  [12] = 'drift',
-  [13] = 'lpf',
-  [14] = 'monitor',
-  [15] = 'amp',
-}
-
-local ccHandlers = {
-  ['hiTexAmp'] = function(val)
-      params:set('hiTexAmp', val)
-      return 'scratch amp ' .. val
-    end,
-  ['bounce'] = function(val)
-    params:set('bounce', val)
-    return 'bounce ' .. val
-    end,
-  ['drift'] = function(val)
-      params:set('drift', val)
-      return 'drift ' .. val
-    end,
-  ['lpf'] = function(val)
-      params:set('lpf', val)
-      return 'lpf ' .. val
-    end,
-  ['monitor'] = function(val)
-      params:set('monitor', val)
-      return 'monitor ' .. val
-    end,
-  ['amp'] = function(val)
-      params:set('amp', val)
-      return 'amp ' .. val
-    end,
-}
 
 function midiEvent(data)
   -- tab.print(midi.to_msg(data))
@@ -122,9 +75,8 @@ function midiEvent(data)
       redraw('stop sweep')
     end
   elseif d.type == 'cc' then
-    local handler = ccAkaiMapping[d.cc]
-    if handler ~= nil and ccHandlers[handler] ~= nil then
-      local msg = ccHandlers[handler](d.val / 127)
+    local handled, msg = BGMidi.handleCCMPD218(MPD218, params, d.cc, d.val)
+    if handled then
       redraw(msg)
     end
   end
@@ -138,6 +90,6 @@ function redraw(msg)
     screen.text('???????')
     screen.update()
   else
-    Hexagon:draw(msg, ccAkaiMapping)
+    Hexagon:drawFancy(MPD218, msg)
   end
 end
