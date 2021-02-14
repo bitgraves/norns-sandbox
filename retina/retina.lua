5601dc380cf424594f8cb6c27a0ff332dc51c170
Ben <spongebucket@gmail.com>
2021-02-15 02:19:23 +0700

    migrate retina

--- retina/retina.lua
+++ retina/retina.lua
@@ -2,45 +2,47 @@
 -- note: uses custom monitor synth
 
 local BGUtil = include('bitgraves/common/bgutil')
+local BGMidi = include('bitgraves/common/bgmidi')
 local Hexagon = include('bitgraves/common/hexagon')
 
 engine.name = 'Retina'
 mid = nil
+local MPD218
 
 function init()
-  audio:rev_off() -- no system reverb
-  audio:pitch_off() -- no system pitch analysis
-  audio:monitor_mono() -- expect only channel 1 input
+  BGUtil.configureSystemStuff()
 
-  params:add_control("amp", "amp", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("amp", function(x)
-    engine.amp(x)
-  end)
-  
-  params:add_control("speed", "speed", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("speed", function(x)
-    engine.speed(util.linlin(0, 1, 1, 2.5, x))
-  end)
-
-  params:add_control("delayAmp", "delayAmp", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("delayAmp", function(x)
-    engine.delayAmp(util.linlin(0, 1, 1, 0, x))
-  end)
-  
-  params:add_control("destroy", "destroy", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("destroy", function(x)
-    engine.destroy(util.linlin(0, 1, 125.0 / 127.0, 0, x))
-  end)
-  
-  params:add_control("sidechainMonitor", "sidechainMonitor", controlspec.new(0, 1, 'lin', 0, 0, ''))
-  params:set_action("sidechainMonitor", function(x)
-    engine.sidechainMonitor(x)
-  end)
+  BGUtil.addEngineControlParam(params, { id = "amp" })
+  BGUtil.addEngineControlParam(params, { id = "speed", min = 1, max = 2.5 })
+  BGUtil.addEngineControlParam(params, {
+    id = "delayAmp",
+    action = function(x)
+      engine.delayAmp(1 - x)
+    end,
+  })
+  BGUtil.addEngineControlParam(params, {
+    id = "destroy",
+    min = -125.0 / 127.0,
+    max = 0,
+    action = function(x)
+      engine.destroy(x * -1)
+    end,
+  })
+  BGUtil.addEngineControlParam(params, { id = "sidechainMonitor" })
 
   params:add_control("monitor", "monitor", controlspec.new(0, 1, 'lin', 0, 0, ''))
   params:set_action("monitor", function(x)
     audio.level_monitor(x)
   end)
+  
+  MPD218 = BGMidi.newInputMappingMPD218({
+    [3] = 'speed',
+    [9] = 'delayAmp',
+    [13] = 'destroy',
+    [14] = 'sidechainMonitor', -- custom monitor synth
+    [15] = 'amp',
+    [20] = 'monitor', -- standard monitor on nonstandard knob
+  })
   
   mid = midi.connect()
   mid.event = midiEvent
@@ -59,43 +61,6 @@ end
 function key(...)
   BGUtil.setlist_key('retina/retina', ...)
 end
-
--- mapping from Akai MPD218 knobs to param handlers
-local ccAkaiMapping = {
-  [3] = 'speed',
-  [9] = 'delayAmp',
-  [13] = 'destroy',
-  [14] = 'sidechainMonitor', -- custom monitor synth
-  [15] = 'amp',
-  [20] = 'monitor', -- standard monitor on nonstandard knob
-}
-
-local ccHandlers = {
-  ['speed'] = function(val)
-      params:set('speed', val)
-      return 'silence ' .. val
-    end,
-  ['delayAmp'] = function(val)
-      params:set('delayAmp', val)
-      return 'delay amp ' .. val
-    end,
-  ['destroy'] = function(val)
-      params:set('destroy', val)
-      return 'destroy ' .. val
-    end,
-  ['sidechainMonitor'] = function(val)
-      params:set('sidechainMonitor', val)
-      return 'minitaur ' .. val
-    end,
-  ['monitor'] = function(val)
-      params:set('monitor', val)
-      return 'monitor ' .. val
-    end,
-  ['amp'] = function(val)
-      params:set('amp', val)
-      return 'amp ' .. val
-    end,
-}
 
 function midiEvent(data)
   -- tab.print(midi.to_msg(data))
@@ -117,14 +82,13 @@ function midiEvent(data)
       engine.noteOff(note)
     end
   elseif d.type == 'cc' then
-    local handler = ccAkaiMapping[d.cc]
-    if handler ~= nil and ccHandlers[handler] ~= nil then
-      local msg = ccHandlers[handler](d.val / 127)
+    local handled, msg = BGMidi.handleCCMPD218(MPD218, params, d.cc, d.val)
+    if handled then
       redraw(msg)
     end
   end
 end
 
 function redraw(msg)
-  Hexagon:draw(msg, ccAkaiMapping)
+  Hexagon:drawFancy(MPD218, msg)
 end
